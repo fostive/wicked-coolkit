36f090bb87f2dc378faeab708f9ee1aa3e03a577
Luke Karrys <lukekarrys@gmail.com>
2020-12-23 15:01:43 -0700

    make dev component

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,14 @@
 import { createElement } from 'lwc';
+import Dev from 'my/dev';
 import MyApp from 'my/app';
 
 window.PRODUCTION = window.location.host.indexOf('localhost') === -1;
 
-const app = createElement('my-app', { is: MyApp });
+let AppComponent = { is: MyApp };
+if (window.location.search === '?dev') {
+    AppComponent = { is: Dev };
+}
+
+const app = createElement('my-app', AppComponent);
 // eslint-disable-next-line @lwc/lwc/no-document-query
 document.querySelector('#main').appendChild(app);
