79115116ed45ef32f7d0519c9ab2c3df9817dfe6
Luke Karrys <lukekarrys@gmail.com>
2021-01-11 15:43:46 -0700

    fix unpkg url

--- src/client/host.js
+++ src/client/host.js
@@ -10,5 +10,5 @@ export const api = (host) => {
 export const sticker = (s) => {
     return isLocal()
         ? `/stickers/svg/${s}.svg`
-        : `https://unpkg.com/wicked-coolkit/stickers/svg/${s}.svg`;
+        : `https://unpkg.com/wicked-coolkit/dist/stickers/svg/${s}.svg`;
 };
